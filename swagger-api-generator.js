5fe98f6334dd2a98844f6bf42fff39ea50fab79b
HandsomeWalker <dreamcreativehub@gmail.com>
2021-12-13 10:06:29 +0800

    Update swagger-api-generator.js

--- swagger-api-generator.js
+++ swagger-api-generator.js
@@ -5,6 +5,7 @@
  * @param fileName 可选，生成文件名，default: swagger-api
  * @param fileType 可选，生成ts还是js，default: ts
  * @param template 可选，生成的ts或者js文件顶部自定义的代码段，default: ''
+ * @param expandParams 可选，是否展开传参，default: false
  * @author HandsomeWalker
  * @example
  * node swagger-api-generator.js url=http://foo/bar tarDir=./foo/bar fileName=service fileType=ts template='import request from "./funcRequest";import QS from "qs";'
@@ -15,7 +16,12 @@ const http = require('http');
 const _path = require('path');
 
 const argvs = process.argv.slice(2);
-let url, tarDir, fileName, fileType, template;
+let url,
+  tarDir,
+  fileName,
+  fileType,
+  template,
+  expandParams = 'false';
 for (const item of argvs) {
   if (/url=.+/g.test(item)) {
     url = item.replace(/url=/g, '');
@@ -32,20 +38,19 @@ for (const item of argvs) {
   if (/template=.+/g.test(item)) {
     template = item.replace(/template=/g, '');
   }
+  if (/expandParams=.+/g.test(item)) {
+    expandParams = item.replace(/expandParams=/g, '');
+  }
 }
 typeof tarDir === 'undefined' && (tarDir = '.');
 typeof fileName === 'undefined' && (fileName = 'swagger-api');
-if (
-  typeof fileType === 'undefined' ||
-  (fileType !== 'ts' && fileType !== 'js')
-) {
+if (typeof fileType === 'undefined' || (fileType !== 'ts' && fileType !== 'js')) {
   fileType = 'ts';
 }
 typeof template === 'undefined' && (template = '');
 let count = 0;
 
-const UA =
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36';
+const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36';
 const config = {
   method: 'GET',
   headers: {
@@ -105,37 +110,122 @@ const responseTypeMap = {
 };
 // 获取每行type字段
 function getTypeField(item) {
-  return `  '${item.name}'${item.required ? '' : '?'}: ${
-    fieldTypeMap[item.type] ? fieldTypeMap[item.type] : 'any'
-  };\n`;
+  return `  '${item.name}'${item.required ? '' : '?'}: ${fieldTypeMap[item.type] ? fieldTypeMap[item.type] : 'any'};\n`;
 }
 // 获取响应字段配置
-function getResponseFields(props) {
-  let resObj = {
-    contentJsDoc: '',
-    contentTypes: '',
-    contentTypesDoc: '',
-  };
-  for (const key in props) {
-    const item = props[key];
-    const { description, type } = item;
-    resObj.contentJsDoc += ` * @returns ${key} description: ${description} | type: ${type}\n`;
-    resObj.contentTypesDoc += ` * @param {${
-      responseTypeMap[type] || 'any'
-    }} ${key} description: ${description} | type: ${type}\n`;
-    resObj.contentTypes += `${key}: ${responseTypeMap[type] || 'any'};\n`;
+function getResponseFields(searchKey, resObj) {
+  let responseContentProps = {};
+  if (searchKey) {
+    responseContentProps = definitionsObj[searchKey];
+    if (responseContentProps) {
+      responseContentProps = responseContentProps.properties || {};
+    }
+  }
+  for (const key in responseContentProps) {
+    const item = responseContentProps[key];
+    if (!item) {
+      continue;
+    }
+    const { description, type, originalRef } = item;
+    if (originalRef) {
+      return getResponseFields(originalRef, resObj);
+    } else {
+      resObj.contentJsDoc += ` * @returns ${key} description: ${description} | type: ${type}\n`;
+      if (!resObj.contentTypesDoc.includes(key)) {
+        resObj.contentTypesDoc += ` * @param {${responseTypeMap[type] || 'any'}} ${key} description: ${description} | type: ${type}\n`;
+      }
+      if (!resObj.contentTypes.includes(key)) {
+        resObj.contentTypes += `${key}: ${responseTypeMap[type] || 'any'};\n`;
+      }
+    }
   }
   return resObj;
+}
+// 获取请求体字段配置
+function getParamsFields({ parameters, data, params, finalComment, finalTypes }) {
+  let hasParams = false;
+  let hasData = false;
+  const parametersType = Object.prototype.toString.call(parameters);
+  if (parametersType === '[object Array]') {
+    // 请求体参数
+    for (const item of parameters) {
+      if (item.schema) {
+        if (item.schema.originalRef) {
+          getParamsFields({
+            parameters: definitionsObj[item.schema.originalRef].properties,
+            data,
+            params,
+            finalComment,
+            finalTypes,
+          });
+          continue;
+        }
+      }
+      if (item.in === 'query') {
+        if (params.includes(`'${item.name}': paramConfig['${item.name}']`)) {
+          continue;
+        }
+        hasParams = true;
+        params += `\t\t'${item.name}': paramConfig['${item.name}'],\n`;
+      } else if (item.in === 'body') {
+        if (data.includes(`'${item.name}': paramConfig['${item.name}']`)) {
+          continue;
+        }
+        hasData = true;
+        data += `\t\t'${item.name}': paramConfig['${item.name}'],\n`;
+      }
+      if (item.in === 'header') {
+        continue;
+      }
+      finalComment += ` * @param {${fieldTypeMap[item.type] ? fieldTypeMap[item.type] : 'any'}} ${item.name} description: ${item.description} | required: ${item.required} | type: ${item.type}\n`;
+      if (item.in === 'path') {
+        continue;
+      }
+      finalTypes += getTypeField(item);
+    }
+  } else if (parametersType === '[object Object]') {
+    for (const key in parameters) {
+      const item = parameters[key];
+      if (item.in === 'query') {
+        if (params.includes(`'${item.name}': paramConfig['${item.name}']`)) {
+          continue;
+        }
+        hasParams = true;
+        params += `\t\t'${item.name}': paramConfig['${item.name}'],\n`;
+      } else if (item.in === 'body') {
+        if (data.includes(`'${item.name}': paramConfig['${item.name}']`)) {
+          continue;
+        }
+        hasData = true;
+        data += `\t\t'${item.name}': paramConfig['${item.name}'],\n`;
+      }
+      if (item.in === 'header') {
+        continue;
+      }
+      finalComment += ` * @param {${fieldTypeMap[item.type] ? fieldTypeMap[item.type] : 'any'}} ${item.name} description: ${item.description} | required: ${item.required} | type: ${item.type}\n`;
+      if (item.in === 'path') {
+        continue;
+      }
+      finalTypes += getTypeField(item);
+    }
+  }
+  return {
+    hasParams,
+    hasData,
+    params,
+    data,
+    finalComment,
+    finalTypes,
+  };
 }
 /**
  * 统一生成模板
  * @param {*} path
  * @param {*} api
- * @param {*} definitionsObj
  * @returns a:电视
  * @returns b:电视放
  */
-function genTemplate(path, api, definitionsObj) {
+function genTemplate(path, api) {
   const names = path.split('/');
   let name = '';
   for (const item of names) {
@@ -155,25 +245,10 @@ function genTemplate(path, api, definitionsObj) {
   const tags = obj.tags[0];
   let { description, parameters, summary, responses, consumes } = obj;
   let showParamConfig = false;
-  let searchKey = '';
-  try {
-    searchKey = responses['200'].schema.originalRef.match(/«[^«»]+»/g)[0];
-  } catch (e) {}
-  let responseContentProps = {};
-  if (searchKey) {
-    searchKey = searchKey.replace(/[«»]/g, '');
-    responseContentProps = definitionsObj[searchKey];
-    if (responseContentProps) {
-      responseContentProps = responseContentProps.properties || {};
-    }
-  }
   // 路径参数
   if (/\{\w+\}/g.test(path)) {
     showParamConfig = true;
-    path = path.replace(
-      /\{(\w+)\}/g,
-      (m, $1) => "${paramConfig['" + $1 + "']}",
-    );
+    path = path.replace(/\{(\w+)\}/g, (m, $1) => "${paramConfig['" + $1 + "']}");
     path = '`' + path + '`';
   } else {
     path = "'" + path + "'";
@@ -187,89 +262,76 @@ function genTemplate(path, api, definitionsObj) {
     }
     if (consumes[0] === 'application/x-www-form-urlencoded') {
       isJsonData = false;
-      data =
-        "\theaders: { 'Content-Type': 'application/x-www-form-urlencoded' },\n" +
-        '  data: QS.stringify({\n';
+      data = "\theaders: { 'Content-Type': 'application/x-www-form-urlencoded' },\n" + '  data: QS.stringify({\n';
     }
   }
   let finalParams = '';
   let finalTypes = '';
   let finalComment = '';
-  let finalResponse = getResponseFields(responseContentProps);
+  let searchKey = '';
+  try {
+    searchKey = responses['200'].schema.originalRef;
+  } catch (e) {}
+  let finalResponse = getResponseFields(searchKey, {
+    contentJsDoc: '',
+    contentTypes: '',
+    contentTypesDoc: '',
+  });
   if (!parameters) {
     parameters = [];
   }
   let hasParams = false;
   let hasData = false;
   // 请求体参数
-  for (const item of parameters) {
-    if (item.in === 'query') {
-      if (params.includes(`'${item.name}': paramConfig['${item.name}']`)) {
-        continue;
-      }
-      hasParams = true;
-      params += `\t\t'${item.name}': paramConfig['${item.name}'],\n`;
-    } else if (item.in === 'body') {
-      if (data.includes(`'${item.name}': paramConfig['${item.name}']`)) {
-        continue;
-      }
-      hasData = true;
-      data += `\t\t'${item.name}': paramConfig['${item.name}'],\n`;
-    }
-    if (item.in === 'header') {
-      continue;
-    }
-    finalComment += ` * @param {${
-      fieldTypeMap[item.type] ? fieldTypeMap[item.type] : 'any'
-    }} ${item.name} description: ${item.description} | required: ${
-      item.required
-    } | type: ${item.type}\n`;
-    if (item.in === 'path') {
-      continue;
-    }
-    finalTypes += getTypeField(item);
-  }
+  const paramsFields = getParamsFields({
+    parameters,
+    data,
+    params,
+    finalComment,
+    finalTypes,
+  });
+  hasParams = paramsFields.hasParams;
+  hasData = paramsFields.hasData;
+  params = paramsFields.params;
+  data = paramsFields.data;
+  finalComment = paramsFields.finalComment;
+  finalTypes = paramsFields.finalTypes;
   params += '\t},\n';
   data += isJsonData ? '\t},\n' : '\t}),\n';
 
   if (hasParams || hasData) {
     showParamConfig = true;
   }
-  hasParams && (finalParams += params);
-  hasData && (finalParams += data);
+  if (expandParams === 'false') {
+    hasParams && (finalParams += '  params: paramConfig,\n');
+    hasData && (finalParams += '  data: paramConfig,\n');
+  } else {
+    hasParams && (finalParams += params);
+    hasData && (finalParams += data);
+  }
 
   return {
     contentJs: `
   /**
    * ${summary}
   ${finalComment}${finalResponse.contentJsDoc}*/
-  export const ${name}${method.toUpperCase()} = (${
-      showParamConfig ? 'paramConfig' : ''
-    }) => request({
+  export const ${name}${method.toUpperCase()} = (paramConfig, customConfig = {}) => request({
     url: ${path},
     method: '${method}',
-  ${finalParams}});
+  ${finalParams}...customConfig,\n});
   `,
     contentTs: `
   /**
    * ${summary}
   ${finalComment}${finalResponse.contentJsDoc}*/
   export const ${name}${method.toUpperCase()} = (${
-      showParamConfig
-        ? 'paramConfig: ' + name + method.toUpperCase() + 'Props'
-        : ''
-    }): ${
-      finalResponse.contentTypes
-        ? 'Promise<' + name + method.toUpperCase() + 'ResProps>'
-        : 'Promise<any>'
-    } => request${
-      finalResponse.contentTypes
-        ? '<' + name + method.toUpperCase() + 'ResProps>'
-        : '<any>'
+      showParamConfig ? 'paramConfig: ' + name + method.toUpperCase() + 'Props' : 'paramConfig?: ' + name + method.toUpperCase() + 'Props'
+    }, customConfig: CustomConfigProps = {}): ${finalResponse.contentTypes ? 'Promise<' + name + method.toUpperCase() + 'ResProps>' : 'Promise<any>'} => request${
+      finalResponse.contentTypes ? '<' + name + method.toUpperCase() + 'ResProps>' : '<any>'
     }({
     url: ${path},
     method: '${method}',
-  ${finalParams}});
+  ${finalParams}...customConfig,\n});
   `,
     contentType: `
   /**
@@ -278,23 +340,14 @@ function genTemplate(path, api, definitionsObj) {
   interface ${name}${method.toUpperCase()}Props extends anyFields {
   ${finalTypes}}
   ${
-    finalResponse.contentTypes
-      ? '/**\n\t' +
-        finalResponse.contentTypesDoc +
-        '*/\ninterface ' +
-        name +
-        method.toUpperCase() +
-        'ResProps{\n' +
-        finalResponse.contentTypes +
-        '}'
-      : ''
+    finalResponse.contentTypes ? '/**\n\t' + finalResponse.contentTypesDoc + '*/\ninterface ' + name + method.toUpperCase() + 'ResProps extends anyFields {\n' + finalResponse.contentTypes + '}' : ''
   }`,
   };
 }
-
+let definitionsObj = {};
 function handleSwaggerApis(data) {
   let contentJs = template;
-  let contentTs = `import './${fileName}Types';\n` + template;
+  let contentTs = `import './${fileName}Types';\n` + template + '\ntype CustomConfigProps = any; // 修改这里为自定义配置支持TS提示\n';
   let contentType = `interface anyFields { [key: string]: any }`;
 
   const jsonPath = `${tarDir}/swagger-apis.json`;
@@ -305,7 +358,7 @@ function handleSwaggerApis(data) {
   let existsData;
 
   const pathObj = data.paths;
-  const definitionsObj = data.definitions;
+  definitionsObj = data.definitions;
 
   if (isExists) {
     existsData = require(jsonPath);
@@ -315,7 +368,7 @@ function handleSwaggerApis(data) {
   }
   let contentObj = {};
   for (const path in pathObj) {
-    contentObj = genTemplate(path, pathObj[path], definitionsObj);
+    contentObj = genTemplate(path, pathObj[path]);
     if (isExists) {
       if (!(path in existsData)) {
         contentJs += contentObj.contentJs;
